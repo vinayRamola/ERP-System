52b43dad9b5203bc3344b45520354dd6022049f9
Vinay Chand Ramola <vinaychandramola123@gmail.com>
2024-04-06 23:30:37 +0530

    add models

--- server/controller/adminControllers.js
+++ server/controller/adminControllers.js
@@ -12,19 +12,18 @@ export const adminLogin = async (req, res) => {
   const errors = { usernameError: String, passwordError: String };
   try {
     const existingAdmin = await Admin.findOne({ username });
-    console.log(existingAdmin)
     if (!existingAdmin) {
       errors.usernameError = "Admin doesn't exist.";
       return res.status(404).json(errors);
     }
-    // const isPasswordCorrect = await bcrypt.compare(
-    //   password,
-    //   existingAdmin.password
-    // );
-    // if (!isPasswordCorrect) {
-    //   errors.passwordError = "Invalid Credentials";
-    //   return res.status(404).json(errors);
-    // }
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      existingAdmin.password
+    );
+    if (!isPasswordCorrect) {
+      errors.passwordError = "Invalid Credentials";
+      return res.status(404).json(errors);
+    }
 
     const token = jwt.sign(
       {
