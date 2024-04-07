70e5a8931505d36d26c09af49e1a67e5c60d7998
Vinay Chand Ramola <vinaychandramola123@gmail.com>
2024-04-07 22:12:31 +0530

    Add faculty routes and controllers

--- server/index.js
+++ server/index.js
@@ -4,17 +4,17 @@ import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 
-// import adminRoutes from "./routes/adminRoutes.js";
+import adminRoutes from "./routes/adminRoutes.js";
 // import studentRoutes from "./routes/studentRoutes.js";
-// import facultyRoutes from "./routes/facultyRoutes.js";
+import facultyRoutes from "./routes/facultyRoutes.js";
 const app = express();
 dotenv.config();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-// app.use("/api/admin", adminRoutes);
-// app.use("/api/faculty", facultyRoutes);
+app.use("/api/admin", adminRoutes);
+app.use("/api/faculty", facultyRoutes);
 // app.use("/api/student", studentRoutes);
 
 const PORT = process.env.PORT || 5000;
